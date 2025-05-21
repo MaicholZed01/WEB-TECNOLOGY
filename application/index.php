28c716dde4b1c98c99aa130e8a33d85df310928d
Davide <davide.lazzarini04@outlook.it>
2025-05-21 17:07:12 +0200

    Creazione cartelle e file template

---
+++ application/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    require "include/template1.inc.php";
+
+    /*
+    $main = new Template("dtml/webarch/index");
+    $body = new Template("dtml/webarch/body");
+
+    $main->setContent("body", $body->get());
+    $main->close();
+    */
+
+    echo "<h1>Prova</h1>";
+
+?>
