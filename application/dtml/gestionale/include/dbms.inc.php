a1e5eb27d795ca27b9b00f5f79dba1e371ded0a3
Davide <davide.lazzarini04@outlook.it>
2025-05-23 17:16:35 +0200

    Aggiornamento credenziali

--- application/dtml/gestionale/include/dbms.inc.php
+++ application/dtml/gestionale/include/dbms.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-	$mysqli = new mysqli("localhost", "root", "viva1felice", "tdw");
+	$mysqli = new mysqli("localhost", "lazzarini21", "", "mylazzarini21");
 
 	if ($mysqli->connect_errno) {
 		printf("Connect failed: %s\n", $mysqli->connect_error);
