a2e30b900002e6b90c03dbe4e394c3d8ff11ac11
MaicholZed01 <138523519+MaicholZed01@users.noreply.github.com>
2025-06-10 13:20:16 +0200

    modifiche form profilo e sistremazione query

--- application/private/profilo.php
+++ application/private/profilo.php
@@ -42,24 +42,36 @@ function handleProfile(&$showForm, &$bodyHtml) {
         $email = $db->real_escape_string($_POST['email']);
         $telefono = $db->real_escape_string($_POST['telefono']);
         $bio = $db->real_escape_string($_POST['bio']);
+        $anni_esperienza = (int)$db->real_escape_string($_POST['anni_esperienza']);
         $tariffa = $db->real_escape_string($_POST['tariffa_oraria']);
         
-        // Gestione upload foto (facoltativo)
-        $fotoPath = '';
-        if (!empty($_FILES['url_foto_profilo']['tmp_name'])) {
-            $tmp = $_FILES['url_foto_profilo']['tmp_name'];
-            $name = basename($_FILES['url_foto_profilo']['name']);
-            $dest = __DIR__ . '/../upload/foto_profilo_' . $fisioId . '_' . $name;
-            move_uploaded_file($tmp, $dest);
-            $fotoPath = $db->real_escape_string('/tec-web/application/' . str_replace($_SERVER['DOCUMENT_ROOT'], '', $dest));
-        }
+      $fotoPath = '';
+if (!empty($_FILES['url_foto_profilo']['tmp_name'])) {
+    $tmp  = $_FILES['url_foto_profilo']['tmp_name'];
+    $name = basename($_FILES['url_foto_profilo']['name']);
+    // Costruisci un nome univoco
+    $filename = "foto_profilo_{$fisioId}_" . preg_replace('/[^a-zA-Z0-9._-]/','_', $name);
+    // Cartella filesystem: application/upload/
+    $uploadDir = dirname(__DIR__) . '/upload';
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0755, true);
+    }
+    $dest = $uploadDir . '/' . $filename;
+    if (move_uploaded_file($tmp, $dest)) {
+        // Path da salvare in DB e usare come src nell'HTML:
+        $fotoPath = '/tec-web/application/upload/' . $filename;
+    } else {
+        // Non riesce a salvare file
+        $message = "<div class='alert alert-warning'>Impossibile caricare l'immagine.</div>";
+    }
+}
         
         // Build UPDATE
         $sql = "UPDATE fisioterapisti SET ";
-        $sql .= "nome='$nome', cognome='$cognome', email='$email', telefono='$telefono', bio='$bio', tariffa_oraria='$tariffa'";
+        $sql .= "nome='$nome', cognome='$cognome', email='$email', telefono='$telefono', bio='$bio', anni_esperienza='$anni_esperienza' ,tariffa_oraria='$tariffa'";
         if ($fotoPath) {
-            $sql .= ", url_foto_profilo='$fotoPath'";
-        }
+    $sql .= ", url_foto_profilo = '" . $db->real_escape_string($fotoPath) . "'";
+}
         $sql .= " WHERE fisioterapista_id=$fisioId";
         
         if ($db->query($sql)) {
@@ -70,7 +82,7 @@ function handleProfile(&$showForm, &$bodyHtml) {
     }
 
     // Recupero dati correnti
-    $res = $db->query("SELECT nome,cognome,email,telefono,bio,tariffa_oraria,url_foto_profilo FROM fisioterapisti WHERE fisioterapista_id=$fisioId");
+    $res = $db->query("SELECT nome,cognome,email,telefono,bio,tariffa_oraria,anni_esperienza,url_foto_profilo FROM fisioterapisti WHERE fisioterapista_id=$fisioId");
     $row = $res->fetch_assoc();
 
     // Costruisco il form
@@ -82,6 +94,7 @@ function handleProfile(&$showForm, &$bodyHtml) {
     $tpl->setContent('old_telefono', htmlspecialchars($row['telefono']));
     $tpl->setContent('old_bio', htmlspecialchars($row['bio']));
     $tpl->setContent('old_tariffa_oraria', htmlspecialchars($row['tariffa_oraria']));
+    $tpl->setContent('old_anni_esperienza', htmlspecialchars($row['anni_esperienza']));
     $tpl->setContent('foto_profilo', htmlspecialchars($row['url_foto_profilo']));
     $tpl->setContent('old_fisioterapista_id', $fisioId);
     
